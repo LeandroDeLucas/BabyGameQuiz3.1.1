d71ff6566e6d57c1ad3ee5641b81a30b7d9afc1f
Daniel Roque <2006roque@gmail.com>
2019-08-28 11:27:24 -0300

    alterado para aparece ou a audiencia ou os baloes

--- Game.gd
+++ Game.gd
@@ -110,13 +110,15 @@ func _verifica(var resposta):
 	#verifica se a opção escolhida pelo jogador é a correta
 	if resposta == respostaCerta:
 		_desabilitaBotoes()
-		move = true
-		$somAudiencia.play()
 		score = score + 1
 		if score >= 4:
 			$timFim.wait_time = 5
 			_baloes()
 			score = 0
+		else:
+			move = true
+			$somAudiencia.play()
+			
 		$timFim.start()
 	else:
 		#resposta errada
