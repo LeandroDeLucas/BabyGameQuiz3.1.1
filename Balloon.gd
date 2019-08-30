09dd28f0bccc98e8ce02322173ebf4a25ef51701
Leandro de Lucas <razori01@METALFOTO.LOCAL>
2019-08-30 10:02:57 -0300

    Melhoria na contagem de cores possíveis para os balões. Os balões caem 20% mais rápido quano estourados

--- Balloon.gd
+++ Balloon.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var poped = false
-var velocity = rand_range(0.5,4.0)
+var velocity = rand_range(0.5,2.5)
 var coresPossiveis = ["ffffff","ec0000","1ed86b","eb15ca","7d3571","3f3eff","49ff23","5698b5","6238c8","f35619","c8f319","ff73c8","c7ff00","ebe832"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,12 +13,12 @@ func _process(delta):
 		$balloon.rect_position.y = $balloon.get_rect().position.y - velocity
 	else:
 		if poped:
-			$balloon.rect_position.y = $balloon.get_rect().position.y + 10
+			$balloon.rect_position.y = $balloon.get_rect().position.y + 12
 	if $balloon.get_rect().position.y < -150 or $balloon.get_rect().position.y > 480:
 		get_parent().remove_child(self)
 		
 func _on_balloon_ready():
-	$balloon.self_modulate = coresPossiveis[randi() % 14]
+	$balloon.self_modulate = coresPossiveis[randi() % coresPossiveis.size()]
 
 func _on_balloon_pressed():
 	$balloon.set_normal_texture(load("res://Assets/Imagens/Animacao/popBalloon.png"))
